ea758824dff2164d252fbb30d8da8f4c7ec6164f
Reynald Bernardino <reynaldbernardino@gmail.com>
2026-06-24 16:13:17 -0400

    Add Google Analytics

--- dna-volleyball-v2/app/layout.tsx
+++ dna-volleyball-v2/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import GoogleAnalytics from "@/components/GoogleAnalytics";
 
 // import BackgroundDNA from "@/components/BackgroundDNA";
 
@@ -79,6 +80,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable}`}
     >
       <body className="text-white">
+        <GoogleAnalytics />
         {/* BackgroundDNA temporarily disabled */}
         {/* <BackgroundDNA /> */}
 
